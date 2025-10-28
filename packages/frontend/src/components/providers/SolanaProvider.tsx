dc4cffdce5f2875df377fd8ce073d9c63f328ea8
GotGjee <suthawee1143@gmail.com>
2025-10-28 13:52:44 +0700

    deploy backend

--- packages/frontend/src/components/providers/SolanaProvider.tsx
+++ packages/frontend/src/components/providers/SolanaProvider.tsx
@@ -5,7 +5,7 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 
 // Import styles
